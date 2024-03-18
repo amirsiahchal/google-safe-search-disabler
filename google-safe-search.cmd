5dd0334720e85460567e1852a91533b0444309f3
Amirsiahchal <93969772+amirsiahchal@users.noreply.github.com>
2024-03-19 01:26:10 +0330

    Update google-safe-search.cmd

--- google-safe-search.cmd
+++ google-safe-search.cmd
@@ -1,8 +1,7 @@
 ::Amirsiahchal
 @echo off
-echo Amirsiahchal /n
+echo Amirsiahchal
 echo 142.250.180.142 google.com >> "C:\Windows\System32\drivers\etc\hosts"
 echo 142.250.180.142 www.google.com >> "C:\Windows\System32\drivers\etc\hosts"
-
 pause
 start http://Amirsiahchal.github.io
