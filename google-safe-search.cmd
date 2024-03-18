f9a13b2c97d572d352742baa17d1faa81d3e4ce9
Amirsiahchal <93969772+amirsiahchal@users.noreply.github.com>
2024-03-19 00:06:46 +0330

    Add files via upload

---
+++ google-safe-search.cmd
@@ -0,0 +1,8 @@
+::Amirsiahchal
+@echo off
+echo Amirsiahchal /n
+echo 142.250.180.142 google.com >> "C:\Windows\System32\drivers\etc\hosts"
+echo 142.250.180.142 www.google.com >> "C:\Windows\System32\drivers\etc\hosts"
+
+pause
+start http://Amirsiahchal.github.io
